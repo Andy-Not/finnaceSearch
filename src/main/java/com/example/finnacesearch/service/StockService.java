d63f051f35ee2729cb95bb3689cba9c149dce9e7
joandyalejo <joandyalejogarcia@outlook.com>
2022-12-12 17:57:46 -0500

    Initial commit

---
+++ src/main/java/com/example/finnacesearch/service/StockService.java
@@ -0,0 +1,14 @@
+package com.example.finnacesearch.service;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * This service will be responsible for gathering data from the api
+ *
+ * @author joandy alejo garcia
+ */
+
+@AllArgsConstructor
+@Service
+public class StockService {
+}
---
+++ src/test/java/com/example/finnacesearch/StockSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finnacesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
